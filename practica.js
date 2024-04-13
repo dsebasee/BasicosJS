4cf79083e034bef786a9b155f16007f457eddb30
DSebas1 <dssebastian42@gmail.com>
2024-04-12 23:32:08 -0500

    Actualizando practica.js

--- practica.js
+++ practica.js
@@ -1,3 +1,9 @@
+//i es menor que 10: La variable i se inicializa en 0 y se incrementa en 1 en cada iteración del bucle. El bucle continuará ejecutándose mientras i sea menor que 10.
+//i no es igual a 3: Esto se expresa como i !== 3. 
+//Significa que el bucle se detendrá si i es igual a 3.
+//El operador !== verifica si dos valores no son iguales en JavaScript.
+//Entonces, el bucle se ejecutará mientras i sea menor que 10 y i no sea igual a 3.
+
 for (let i = 0; i < 10 && i !== 3; i++) {
     console.log('Hola')
   }
